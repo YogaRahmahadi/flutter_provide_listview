b8a3e061a4cf4ec0383c2816e7d2aacb80d9ab15
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2022-11-02 15:07:14 +0700

    UTS nomer 2

--- lib/page/addtask.dart
+++ lib/page/addtask.dart
@@ -11,15 +11,62 @@ class AddTaskPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Tambah Task Baru"),
       ),
-      body: Padding(
+      body: const MyTextForm(),
+    );
+  }
+}
+
+class MyTextForm extends StatefulWidget {
+  const MyTextForm({super.key});
+
+  @override
+  MyTextFormState createState() {
+    return MyTextFormState();
+  }
+}
+
+class MyTextFormState extends State<MyTextForm> {
+  final TextEditingController _textName = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var _isEnabled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             TextFormField(
+              controller: _textName,
               decoration: const InputDecoration(
                 hintText: "Masukkan Task Baru",
               ),
+              keyboardType: TextInputType.text,
+              validator: (String? value) {
+                if (value != '') {
+                  if (value!.length < 3) {
+                    return 'Inputan harus lebih dari 3';
+                  } else {
+                    return null;
+                  }
+                } else {
+                  return "Inputan haus diisi!";
+                }
+              },
               onChanged: (value) {
+                setState(() {
+                  if (_textName.text.isNotEmpty) {
+                    if (value.length < 3) {
+                      _isEnabled = false;
+                    } else {
+                      _isEnabled = true;
+                    }
+                  } else {
+                    _isEnabled = false;
+                  }
+                });
                 context.read<Tasklist>().changeTaskName(value);
               },
             ),
@@ -30,10 +77,19 @@ class AddTaskPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      context.read<Tasklist>().addTask();
-                      Navigator.pop(context);
-                    },
+                    onPressed: _isEnabled
+                        ? () {
+                            if (_textName.text.isNotEmpty) {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+                                context.read<Tasklist>().addTask();
+                                Navigator.pop(context);
+                              }
+                            } else {
+                              return;
+                            }
+                          }
+                        : null,
                     child: const Text("Tambah Task Baru"),
                   ),
                 ),
